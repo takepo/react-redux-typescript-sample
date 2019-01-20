fd85dbb9593c2932c799703bd2933fbce1aa50d4
takepo <takeshima.nahoko@lancers.co.jp>
2019-01-21 01:00:55 +0900

    feat: eact-hello-worldの追加

---
+++ react-hello-world/src/app.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+class App extends React.Component<{}, {}> {
+    render(): JSX.Element {
+        return (
+            <div>HelloWorld!</div>
+        );
+    }
+}
+
+export default App;
+
